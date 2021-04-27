77abf2f0fb1d180039a0b3ae7f9b5b4f59dc330d
Juan Cruz Ortega <juancruzortegacoldorf@gmail.com>
2021-04-27 18:41:07 -0300

    FullDBConn

--- app.py
+++ app.py
@@ -27,19 +27,13 @@ class Pet(db.Model):
     name = db.Column(db.String,unique=True,nullable=False)
     age = db.Column(db.Integer)
     bio = db.Column(db.String(300))
-    posted_by = db.Column(db.String,db.ForeignKey('user.id'),nullable=False)
+    posted_by = db.Column(db.String,db.ForeignKey('user.id'))
     
 
 
 db.create_all()
 
-
-
-
-
-
-
-"""Information regarding the Pets in the System."""
+'''
 pets = [
             {"id": 1, "name": "Nelly", "age": "5 weeks", "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
             {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
@@ -47,10 +41,23 @@ pets = [
             {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."}, 
         ]
 
-"""Information regarding the Users in the System."""
-users = [
-            {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.co", "password": "adminpass"},
-        ]
+new_pet1 = Pet(name="Nelly",age="5 weeks",bio="I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles.")
+new_pet2 = Pet(name="Yuki",age="8 month",bio="I am a handsome gentle-cat. I like to dress up in bow ties.")
+new_pet3 = Pet(name="Basker",age="1 year",bio="I love barking. But, I love my friends more.")
+new_pet4 = Pet(name="Mr. Furrkins",age="5 years",bio="Probably napping.")
+db.session.add(new_pet1)
+db.session.add(new_pet2)
+db.session.add(new_pet3)
+db.session.add(new_pet4)
+try:
+    db.session.commit()
+except Exception as e: 
+    print(e)
+    db.session.rollback()
+finally:
+    db.session.close() '''
+
+
 
 
 
@@ -61,8 +68,9 @@ users = [
 def login():
     form=loginForm()   
     if(form.validate_on_submit()):
-        if([u for  u in users if u["email"]==form.email.data and u["password"]==form.password.data]):
-            session["user"]=[u for  u in users if u["email"]==form.email.data and u["password"]==form.password.data]
+        user_log = User.query.filter_by(email=form.email.data).filter_by(password=form.password.data).first()
+        if(user_log):
+            session["user"] = user_log.full_name
             return render_template("login.html",message="Succesfully Logged In");
         else:
             return render_template("login.html",message="Wrong credentials. Please try again.");
@@ -82,27 +90,19 @@ def logout():
 def signUp():
     form=signUpForm()   
     if(form.validate_on_submit()):
-            user_sign : list = [x for x in users if x["email"]==form.email.data]
-            if len(user_sign)==0:
-                seq = [x['id'] for x in users]
-                new_id :int = max(seq) + 1
-                new_user: dict = {
-                    "id": new_id , "full_name": form.fullName.data , "email": form.email.data, "password":form.password.data 
-                }
-                users.append(new_user)
-                ''' 
-                    db.session.add(new_user)
-                    try:
-                        db.session.commit()
-                    except Exception as e: 
-                        db.session.rollback()
-                    finally:
-                        db.session.close()
-                '''
-                print(users)
+            user_query = User.query.filter_by(email = form.email.data).first()
+            if not user_query:
+                new_user = User(full_name=form.fullName.data ,email= form.email.data,password=form.password.data)
+                db.session.add(new_user)
+                try:
+                    db.session.commit()
+                except Exception as e: 
+                    db.session.rollback()
+                finally:
+                    db.session.close()
                 return render_template("signup.html",message="User Successfully Created");
-            elif len(user_sign)!=0:
-                return render_template("signup.html",message="This User has alredy been created");
+            elif user_query:
+                return render_template("signup.html",message="This email already exists in the system! Please log in instead.");
     elif form.errors:
         return render_template("signup.html",form=form);
     return render_template("signup.html",form=form);
@@ -110,7 +110,8 @@ def signUp():
 
 @app.route('/')
 def home():
-    return render_template("home.html",pets=pets);
+    all_pets=Pet.query.all()
+    return render_template("home.html",pets=all_pets);
 
 @app.route('/about')
 def about():
@@ -119,7 +120,7 @@ def about():
 
 @app.route('/details/<int:id>')
 def details(id):
-    pet = next((pet for pet in pets if pet["id"] == id), None) 
+    pet = Pet.query.get(id) 
     if pet is None: 
         abort(404, description="No Pet was Found with the given ID")
     return render_template("details.html", pet = pet)
